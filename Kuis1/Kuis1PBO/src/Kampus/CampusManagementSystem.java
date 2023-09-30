262b93347b73caf143976733a9dece733927568d
Arucaden <daffamaulanasatria@gmail.com>
2023-09-30 12:52:04 +0700

    Update CMS
    Warapped the case into a method

--- Kuis1/Kuis1PBO/src/Kampus/CampusManagementSystem.java
+++ Kuis1/Kuis1PBO/src/Kampus/CampusManagementSystem.java
@@ -5,8 +5,182 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CampusManagementSystem {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static void addStudent(List<Student> students) {
+        System.out.print("Nama Mahasiswa: ");
+        String studentName = scanner.nextLine();
+        System.out.print("Usia Mahasiswa: ");
+        int studentAge = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("NIM Mahasiswa: ");
+        String studentId = scanner.nextLine();
+        Student student = new Student(studentName, studentAge, studentId);
+        students.add(student);
+        System.out.println("Mahasiswa berhasil ditambahkan!");
+    }
+
+    static void addDosen(List<Faculty> faculties) {
+        System.out.print("Nama Dosen: ");
+        String facultyName = scanner.nextLine();
+        System.out.print("Usia Dosen: ");
+        int facultyAge = scanner.nextInt();
+        scanner.nextLine(); 
+        System.out.print("ID Dosen: ");
+        String facultyId = scanner.nextLine();
+        Faculty faculty = new Faculty(facultyName, facultyAge, facultyId);
+        faculties.add(faculty);
+        System.out.println("Dosen berhasil ditambahkan!");
+    }
+
+    static void addCourse(List<Course> courses) {
+        System.out.print("ID Mata Kuliah: ");
+        String courseId = scanner.nextLine();
+        System.out.print("Nama Mata Kuliah: ");
+        String courseName = scanner.nextLine();
+        Course course = new Course(courseId, courseName);
+        courses.add(course);
+        System.out.println("Mata Kuliah berhasil ditambahkan!");
+    }
+
+    static void addClassroom(List<Classroom> classrooms) {
+        System.out.print("ID Ruang Kelas: ");
+        String classroomId = scanner.nextLine();
+        System.out.print("Lokasi Ruang Kelas: ");
+        String location = scanner.nextLine();
+        Classroom classroom = new Classroom(classroomId, location);
+        classrooms.add(classroom);
+        System.out.println("Ruang Kelas berhasil ditambahkan!");
+    }
+
+    static void addEnrollment(List<Student> students, List<Course> courses, List<Enrollment> enrollments) {
+        System.out.print("Pilih Mahasiswa (NIM): ");
+        String studentIdForEnrollment = scanner.nextLine();
+        System.out.print("Pilih Mata Kuliah (ID): ");
+        String courseIdForEnrollment = scanner.nextLine();
+
+        // Cari mahasiswa berdasarkan ID
+        Student selectedStudentForEnrollment = null;
+        for (Student s : students) {
+            if (s.getStudentId().equals(studentIdForEnrollment)) {
+                selectedStudentForEnrollment = s;
+                break;
+            }
+        }
+
+        // Cari mata kuliah berdasarkan ID
+        Course selectedCourseForEnrollment = null;
+        for (Course c : courses) {
+            if (c.getCourseId().equals(courseIdForEnrollment)) {
+                selectedCourseForEnrollment = c;
+                break;
+            }
+        }
+
+        if (selectedStudentForEnrollment != null && selectedCourseForEnrollment != null) {
+            Enrollment enrollment = new Enrollment(selectedStudentForEnrollment, selectedCourseForEnrollment);
+            enrollments.add(enrollment);
+            System.out.println("Pendaftaran berhasil!");
+        } else {
+            System.out.println("Mahasiswa atau mata kuliah tidak ditemukan.");
+        }
+    }
+
+    static void addFacultyAssignment(List<Faculty> faculties, List<Course> courses, List<FacultyAssignment> facultyAssignments) {
+        System.out.print("Pilih Dosen (ID): ");
+        String facultyIdForAssignment = scanner.nextLine();
+        System.out.print("Pilih Mata Kuliah (ID): ");
+        String courseIdForAssignment = scanner.nextLine();
+
+        // Cari dosen berdasarkan ID
+        Faculty selectedFacultyForAssignment = null;
+        for (Faculty f : faculties) {
+            if (f.getEmployeeId().equals(facultyIdForAssignment)) {
+                selectedFacultyForAssignment = f;
+                break;
+            }
+        }
+
+        // Cari mata kuliah berdasarkan ID
+        Course selectedCourseForAssignment = null;
+        for (Course c : courses) {
+            if (c.getCourseId().equals(courseIdForAssignment)) {
+                selectedCourseForAssignment = c;
+                break;
+            }
+        }
+
+        if (selectedFacultyForAssignment != null && selectedCourseForAssignment != null) {
+            FacultyAssignment facultyAssignment = new FacultyAssignment(selectedFacultyForAssignment,
+                    selectedCourseForAssignment);
+            facultyAssignments.add(facultyAssignment);
+            System.out.println("Penugasan dosen berhasil!");
+        } else {
+            System.out.println("Dosen atau mata kuliah tidak ditemukan.");
+        }
+    }
+
+    static void addTimetable(List<Classroom> classrooms, List<Course> courses, List<Timetable> timetables) {
+        System.out.print("Pilih Ruang Kelas (ID): ");
+        String classroomIdForTimetable = scanner.nextLine();
+        System.out.print("Pilih Mata Kuliah (ID): ");
+        String courseIdForTimetable = scanner.nextLine();
+        System.out.print("Hari Kuliah: ");
+        String day = scanner.nextLine();
+        System.out.print("Waktu Kuliah: ");
+        String time = scanner.nextLine();
+
+        // Cari ruang kelas berdasarkan ID
+        Classroom selectedClassroomForTimetable = null;
+        for (Classroom c : classrooms) {
+            if (c.getClassroomId().equals(classroomIdForTimetable)) {
+                selectedClassroomForTimetable = c;
+                break;
+            }
+        }
+
+        // Cari mata kuliah berdasarkan ID
+        Course selectedCourseForTimetable = null;
+        for (Course c : courses) {
+            if (c.getCourseId().equals(courseIdForTimetable)) {
+                selectedCourseForTimetable = c;
+                break;
+            }
+        }
+
+        if (selectedClassroomForTimetable != null && selectedCourseForTimetable != null) {
+            Timetable timetable = new Timetable(selectedClassroomForTimetable, selectedCourseForTimetable, day, time);
+            timetables.add(timetable);
+            System.out.println("Penjadwalan kuliah berhasil!");
+        } else {
+            System.out.println("Ruang kelas atau mata kuliah tidak ditemukan.");
+        }
+    }
+
+    static void accessStudentData(List<Student> students) {
+        System.out.print("Masukkan NIM Mahasiswa: ");
+        String nimToSearch = scanner.nextLine();
+
+        // Cari mahasiswa berdasarkan NIM
+        boolean found = false;
+        for (Student s : students) {
+            if (s.getStudentId().equals(nimToSearch)) {
+                s.displayInfo(); // Menampilkan informasi mahasiswa
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Mahasiswa dengan NIM " + nimToSearch + " tidak ditemukan.");
+        }
+    }
+    
+//===================================================================================================
+//===================================================================================================
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         List<Student> students = new ArrayList<>();
         List<Faculty> faculties = new ArrayList<>();
         List<Course> courses = new ArrayList<>();
@@ -32,175 +206,39 @@ public class CampusManagementSystem {
 
             switch (choice) {
                 case 1:
-                    System.out.print("Nama Mahasiswa: ");
-                    String studentName = scanner.nextLine();
-                    System.out.print("Usia Mahasiswa: ");
-                    int studentAge = scanner.nextInt();
-                    scanner.nextLine();  // Membaca karakter newline setelah input usia
-                    System.out.print("NIM Mahasiswa: ");
-                    String studentId = scanner.nextLine();
-                    Student student = new Student(studentName, studentAge, studentId);
-                    students.add(student);
-                    System.out.println("Mahasiswa berhasil ditambahkan!");
+                    addStudent(students);
                     break;
 
                 case 2:
-                    System.out.print("Nama Dosen: ");
-                    String facultyName = scanner.nextLine();
-                    System.out.print("Usia Dosen: ");
-                    int facultyAge = scanner.nextInt();
-                    scanner.nextLine();  // Membaca karakter newline setelah input usia
-                    System.out.print("ID Dosen: ");
-                    String facultyId = scanner.nextLine();
-                    Faculty faculty = new Faculty(facultyName, facultyAge, facultyId);
-                    faculties.add(faculty);
-                    System.out.println("Dosen berhasil ditambahkan!");
+                    addDosen(faculties);
                     break;
 
                 case 3:
-                    System.out.print("ID Mata Kuliah: ");
-                    String courseId = scanner.nextLine();
-                    System.out.print("Nama Mata Kuliah: ");
-                    String courseName = scanner.nextLine();
-                    Course course = new Course(courseId, courseName);
-                    courses.add(course);
-                    System.out.println("Mata Kuliah berhasil ditambahkan!");
+                    addCourse(courses);
                     break;
 
                 case 4:
-                    System.out.print("ID Ruang Kelas: ");
-                    String classroomId = scanner.nextLine();
-                    System.out.print("Lokasi Ruang Kelas: ");
-                    String location = scanner.nextLine();
-                    Classroom classroom = new Classroom(classroomId, location);
-                    classrooms.add(classroom);
-                    System.out.println("Ruang Kelas berhasil ditambahkan!");
+                    addClassroom(classrooms);
                     break;
 
                 case 5:
-                    System.out.print("Pilih Mahasiswa (NIM): ");
-                    String studentIdForEnrollment = scanner.nextLine();
-                    System.out.print("Pilih Mata Kuliah (ID): ");
-                    String courseIdForEnrollment = scanner.nextLine();
-
-                    // Cari mahasiswa berdasarkan ID
-                    Student selectedStudentForEnrollment = null;
-                    for (Student s : students) {
-                        if (s.getStudentId().equals(studentIdForEnrollment)) {
-                            selectedStudentForEnrollment = s;
-                            break;
-                        }
-                    }
-
-                    // Cari mata kuliah berdasarkan ID
-                    Course selectedCourseForEnrollment = null;
-                    for (Course c : courses) {
-                        if (c.getCourseId().equals(courseIdForEnrollment)) {
-                            selectedCourseForEnrollment = c;
-                            break;
-                        }
-                    }
-
-                    if (selectedStudentForEnrollment != null && selectedCourseForEnrollment != null) {
-                        Enrollment enrollment = new Enrollment(selectedStudentForEnrollment, selectedCourseForEnrollment);
-                        enrollments.add(enrollment);
-                        System.out.println("Pendaftaran berhasil!");
-                    } else {
-                        System.out.println("Mahasiswa atau mata kuliah tidak ditemukan.");
-                    }
+                    addEnrollment(students, courses, enrollments);
                     break;
 
                 case 6:
-                    System.out.print("Pilih Dosen (ID): ");
-                    String facultyIdForAssignment = scanner.nextLine();
-                    System.out.print("Pilih Mata Kuliah (ID): ");
-                    String courseIdForAssignment = scanner.nextLine();
-
-                    // Cari dosen berdasarkan ID
-                    Faculty selectedFacultyForAssignment = null;
-                    for (Faculty f : faculties) {
-                        if (f.getEmployeeId().equals(facultyIdForAssignment)) {
-                            selectedFacultyForAssignment = f;
-                            break;
-                        }
-                    }
-
-                    // Cari mata kuliah berdasarkan ID
-                    Course selectedCourseForAssignment = null;
-                    for (Course c : courses) {
-                        if (c.getCourseId().equals(courseIdForAssignment)) {
-                            selectedCourseForAssignment = c;
-                            break;
-                        }
-                    }
-
-                    if (selectedFacultyForAssignment != null && selectedCourseForAssignment != null) {
-                        FacultyAssignment facultyAssignment = new FacultyAssignment(selectedFacultyForAssignment, selectedCourseForAssignment);
-                        facultyAssignments.add(facultyAssignment);
-                        System.out.println("Penugasan dosen berhasil!");
-                    } else {
-                        System.out.println("Dosen atau mata kuliah tidak ditemukan.");
-                    }
+                    addFacultyAssignment(faculties, courses, facultyAssignments);
                     break;
 
                 case 7:
-                    System.out.print("Pilih Ruang Kelas (ID): ");
-                    String classroomIdForTimetable = scanner.nextLine();
-                    System.out.print("Pilih Mata Kuliah (ID): ");
-                    String courseIdForTimetable = scanner.nextLine();
-                    System.out.print("Hari Kuliah: ");
-                    String day = scanner.nextLine();
-                    System.out.print("Waktu Kuliah: ");
-                    String time = scanner.nextLine();
-
-                    // Cari ruang kelas berdasarkan ID
-                    Classroom selectedClassroomForTimetable = null;
-                    for (Classroom c : classrooms) {
-                        if (c.getClassroomId().equals(classroomIdForTimetable)) {
-                            selectedClassroomForTimetable = c;
-                            break;
-                        }
-                    }
-
-                    // Cari mata kuliah berdasarkan ID
-                    Course selectedCourseForTimetable = null;
-                    for (Course c : courses) {
-                        if (c.getCourseId().equals(courseIdForTimetable)) {
-                            selectedCourseForTimetable = c;
-                            break;
-                        }
-                    }
-
-                    if (selectedClassroomForTimetable != null && selectedCourseForTimetable != null) {
-                        Timetable timetable = new Timetable(selectedClassroomForTimetable, selectedCourseForTimetable, day, time);
-                        timetables.add(timetable);
-                        System.out.println("Penjadwalan kuliah berhasil!");
-                    } else {
-                        System.out.println("Ruang kelas atau mata kuliah tidak ditemukan.");
-                    }
+                    addTimetable(classrooms, courses, timetables);
                     break;
 
                 case 8:
-                    System.out.print("Masukkan NIM Mahasiswa: ");
-                    String nimToSearch = scanner.nextLine();
-
-                    // Cari mahasiswa berdasarkan NIM
-                    boolean found = false;
-                    for (Student s : students) {
-                        if (s.getStudentId().equals(nimToSearch)) {
-                            s.displayInfo(); // Menampilkan informasi mahasiswa
-                            found = true;
-                            break;
-                        }
-                    }
-
-                    if (!found) {
-                        System.out.println("Mahasiswa dengan NIM " + nimToSearch + " tidak ditemukan.");
-                    }
+                    accessStudentData(students);
                     break;
 
                 case 9:
-                    System.out.println("Terima kasih! Program selesai.");
+                    System.out.println("Terima kasih telah menggunakan program ini!.");
                     scanner.close();
                     System.exit(0);
 
